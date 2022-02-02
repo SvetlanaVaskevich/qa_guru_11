c0d5be64e671f86dc8b9fbfe94c728bef7771e36
Svetlana Vaskevich <svaskevich@bk.ru>
2022-02-03 00:09:46 +0500

    Added condition HoffSearchTest

--- src/test/java/vaskevich/assertions_test/HoffSearchTest.java
+++ src/test/java/vaskevich/assertions_test/HoffSearchTest.java
@@ -1,4 +1,17 @@
 package vaskevich.assertions_test;
 
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class HoffSearchTest {
+
+    @Test
+    public void searchHoffProduct(){
+        open("https://hoff.ru/");
+        $("input[name='search']").setValue("гостиная");
+        $("h1.page-title").shouldHave(Condition.text("Гостиная"));
+    }
 }
