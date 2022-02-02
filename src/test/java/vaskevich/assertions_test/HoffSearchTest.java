a4534fc21360780e27129541add5a2bbcecae71b
SvetlanaVaskevich <81557049+SvetlanaVaskevich@users.noreply.github.com>
2022-02-03 00:36:21 +0500

    Added condition HoffSearchTest (#3)
    * Added condition HoffSearchTest
    * Added test commits in HoffSearchTest

--- src/test/java/vaskevich/assertions_test/HoffSearchTest.java
+++ src/test/java/vaskevich/assertions_test/HoffSearchTest.java
@@ -1,4 +1,20 @@
 package vaskevich.assertions_test;
 
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class HoffSearchTest {
+
+    @Test
+    public void searchHoffProduct(){
+        //открываем главную страницу
+        open("https://hoff.ru/");
+        //в поле поиска заносим товар
+        $("input[name='search']").setValue("гостиная");
+        //проверяем, что получили запрашиваемую категорию товара
+        $("h1.page-title").shouldHave(Condition.text("Гостиная"));
+    }
 }
