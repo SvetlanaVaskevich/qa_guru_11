6536d005f1241fe5d6de4f96edbba67b7520a71c
Svetlana Vaskevich <svaskevich@bk.ru>
2022-02-02 14:25:25 +0500

    IkeaSearchTest test added

--- src/test/java/vaskevich/assertions_test/IkeaSearchTest.java
+++ src/test/java/vaskevich/assertions_test/IkeaSearchTest.java
@@ -1,5 +1,6 @@
 package vaskevich.assertions_test;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.$;
@@ -11,6 +12,7 @@ public class IkeaSearchTest {
     public void searchIkeaProductTest(){
         open("https://www.ikea.com/ru/ru/");
         $("input[type='search']").setValue("стол").pressEnter();
+        $("b.notranslate").shouldHave(Condition.text("стол"));
     }
 
 }
