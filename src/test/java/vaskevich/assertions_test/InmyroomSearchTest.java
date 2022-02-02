1d1743fff85d57c8fd04e7ab6dad54055bf19a58
Svetlana Vaskevich <svaskevich@bk.ru>
2022-02-02 14:28:43 +0500

    InmyroomSearchTest test added

--- src/test/java/vaskevich/assertions_test/InmyroomSearchTest.java
+++ src/test/java/vaskevich/assertions_test/InmyroomSearchTest.java
@@ -1,5 +1,6 @@
 package vaskevich.assertions_test;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.$;
@@ -11,5 +12,6 @@ public class InmyroomSearchTest {
     public void searchInmyroomProductTest(){
         open("https://www.inmyroom.ru/");
         $("input[type='text']").setValue("стол").pressEnter();
+        $("div.s-Search_b-Title").$("h1").shouldHave(Condition.text("стол"));
     }
 }
